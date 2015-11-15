c3b0b7e96e5a2eb175d151511c78d2dc9f8977b4
Kota Shiratsuka <kota@insaneworks.co.jp>
2015-11-15 10:47:29 +0900

    Fix noupdate origin task Subjects/Descriptios

--- lib/scheduled_tasks_checker.rb
+++ lib/scheduled_tasks_checker.rb
@@ -4,27 +4,36 @@ class ScheduledTasksChecker
 
       # replace variables (set locale from shell)
       I18n.locale = ENV['LOCALE'] || I18n.default_locale
-      task.subject.gsub!('**WEEK**', Time.now.strftime("%W"))
-      task.subject.gsub!('**MONTH**', Time.now.strftime("%m"))
-      task.subject.gsub!('**MONTHNAME**', I18n.localize(Time.now, :format => "%B"))
-      task.subject.gsub!('**YEAR**', Time.now.strftime("%Y"))
-      task.subject.gsub!('**PREVIOUS_MONTHNAME**', I18n.localize(Time.now - 2592000, :format => "%B"))
-      task.description.gsub!('**WEEK**', Time.now.strftime("%W"))
-      task.description.gsub!('**MONTH**', Time.now.strftime("%m"))
-      task.description.gsub!('**MONTHNAME**', I18n.localize(Time.now, :format => "%B"))
-      task.description.gsub!('**YEAR**', Time.now.strftime("%Y"))
-      task.description.gsub!('**PREVIOUS_MONTHNAME**', I18n.localize(Time.now - 2592000, :format => "%B"))
 
-      print "assigning #{task.subject}\n"
-      issue = Issue.new(:project_id=>task.project_id,  :tracker_id=>task.tracker_id, :category_id=>task.issue_category_id, :assigned_to_id=>task.assigned_to_id, :author_id=>task.author_id, :subject=>task.subject, :description=>task.description);
+      issue = Issue.new(
+                        :project_id=>task.project_id,
+                        :tracker_id=>task.tracker_id,
+                        :category_id=>task.issue_category_id,
+                        :assigned_to_id=>task.assigned_to_id,
+                        :author_id=>task.author_id,
+                        :subject=>task.subject,
+                        :description=>task.description
+      );
+
+      issue.subject.gsub!('**WEEK**', Time.now.strftime("%W"))
+      issue.subject.gsub!('**MONTH**', Time.now.strftime("%m"))
+      issue.subject.gsub!('**MONTHNAME**', I18n.localize(Time.now, :format => "%B"))
+      issue.subject.gsub!('**YEAR**', Time.now.strftime("%Y"))
+      issue.subject.gsub!('**PREVIOUS_MONTHNAME**', I18n.localize(Time.now - 2592000, :format => "%B"))
+
+      issue.description.gsub!('**WEEK**', Time.now.strftime("%W"))
+      issue.description.gsub!('**MONTH**', Time.now.strftime("%m"))
+      issue.description.gsub!('**MONTHNAME**', I18n.localize(Time.now, :format => "%B"))
+      issue.description.gsub!('**YEAR**', Time.now.strftime("%Y"))
+      issue.description.gsub!('**PREVIOUS_MONTHNAME**', I18n.localize(Time.now - 2592000, :format => "%B"))
+
       issue.start_date ||= Date.today if task.set_start_date?
+
       if task.due_date_number
         due_date = task.due_date_number
         due_date_units = task.due_date_units
         issue.due_date = due_date.send(due_date_units.downcase).from_now
       end
-
-      issue.save
 
       print "assigning #{issue.subject}\n"
       issue.save
@@ -32,11 +41,13 @@ class ScheduledTasksChecker
       interval = task.interval_number
       units = task.interval_units
       interval_steps = ((Time.now - task.next_run_date) / interval.send(units.downcase)).ceil
+
       task.next_run_date += (interval * interval_steps).send(units.downcase)
 
       print "next #{task.next_run_date}\n"
       task.save
 
     end
+
   end
 end
